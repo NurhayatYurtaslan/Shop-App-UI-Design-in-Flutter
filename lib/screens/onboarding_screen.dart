eb3b03f4884ff6c61ad90c445bda7e89be80de35
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-09-02 23:47:36 +0300

    add ✨ : onboarding pages

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.w700,
+      ),
+      bodyTextStyle: TextStyle(fontSize: 19),
+      bodyPadding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      pages: [
+        PageViewModel(
+          title: "Shop Now",
+          body:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: Image.asset(
+            "assets/images/splash1.png",
+            width: 200,
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Big Discount",
+          body:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: Image.asset(
+            "assets/images/splash2.png",
+            width: 200,
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: " Free Delivery",
+          body:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: Image.asset(
+            "assets/images/splash3.png",
+            width: 200,
+          ),
+          decoration: pageDecoration,
+        ),
+      ],
+      showSkipButton: false,
+      showDoneButton: false,
+      showBackButton: true,
+      back: Text(
+        "Back",
+        style:
+            TextStyle(fontWeight: FontWeight.w600, color: Color(0xFEFFF6969)),
+      ),
+      next: Text(
+        "Next",
+        style:
+            TextStyle(fontWeight: FontWeight.w600, color: Color(0xFEFFF6969)),
+      ),
+      onDone: () {},
+      onSkip: () {},
+      dotsDecorator: DotsDecorator(
+        size: Size.square(10),
+        activeSize: Size(20, 10),
+        activeColor: Color(0xFEFFF6969),
+        color: Colors.black26,
+        spacing: EdgeInsets.symmetric(horizontal: 3),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+      // Pass the page decoration to the IntroductionScreen
+    );
+  }
+}
